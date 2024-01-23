c9539ade0b182b8722d10609d592383ec7091679
s-deep3k <sankhadeep.genx@gmail.com>
2024-01-24 01:16:31 +0530

    feat: Appended to Array

--- functions/src/app/index.ts
+++ functions/src/app/index.ts
@@ -3,6 +3,11 @@ import { NODE_ENV, PORT} from "../config";
 import {OpenApi} from "../config/openai";
 import bodyParser from "body-parser";
 
+interface FivePosts{
+  title?: string,
+  content?: string
+}
+
 export default class Server {
   private static getInstance: Express;
   private app: Express;
@@ -39,17 +44,31 @@ export default class Server {
           content: `
           Write 5 social media posts about the product "${productName}" 
           with product motto "${productMoto}" 
-          for Build in Public purpose.(Include emojis and Post Title)`,
+          for Build in Public purpose.
+          (Include emojis and Post)`,
         }],
         model: "gpt-3.5-turbo",
         stream: false,
         temperature: 0.1,
         max_tokens: 500,
       });
-      console.log(description.choices[0].message.content);
+      const lines = description.choices[0].message.content
+        ?.split("\n").filter((line)=>line.trim()!=="");
+      console.log(lines);
+      const fivePosts=[];
+      lines?.forEach((val, index)=>{
+        const temp:FivePosts = {};
+        if (index%2==0) {
+          temp["title"]=val;
+        }
+        if (index%2!=0) {
+          temp["content"]=val;
+        }
+        fivePosts.push(temp);
+      });
       res
         .status(200)
-        .send(JSON.stringify(description.choices[0].message.content));
+        .send(description.choices[0].message.content);
     });
     if (NODE_ENV === "development") {
       this.app.listen(port, () => {
